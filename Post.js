12d5f748e3eb1cf96cd439edaeebee6dbeae913f
maciek367 <maciek2baba2@gmail.com>
2023-08-23 23:15:43 +0200

    add db and Post class

---
+++ Post.js
@@ -0,0 +1,40 @@
+export class Post {
+  constructor(id, content, title, author) {
+    this.id = id;
+    this.content = content;
+    this.title = title;
+    this.author = author;
+  }
+
+  getId() {
+    return this.id;
+  }
+
+  setId(id) {
+    this.id = id;
+  }
+
+  getContent() {
+    return this.content;
+  }
+
+  setContent(content) {
+    this.content = content;
+  }
+
+  getTitle() {
+    return this.title;
+  }
+
+  setTitle(title) {
+    this.title = title;
+  }
+
+  getAuthor() {
+    return this.author;
+  }
+
+  setAuthor(author) {
+    this.author = author;
+  }
+}
---
+++ db.js
@@ -0,0 +1,144 @@
+let postsData = [
+  {
+    id: 1,
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel erat in lorem luctus aliquet. Vivamus eleifend felis a ipsum facilisis ullamcorper.",
+    title: "The Art of Programming",
+    author: "CodingMaster",
+  },
+  {
+    id: 2,
+    content:
+      "In today's world, technology plays a crucial role. From smartphones to smart homes, we are surrounded by innovations that make our lives more convenient.",
+    title: "Embracing the Digital Age",
+    author: "TechEnthusiast",
+  },
+  {
+    id: 3,
+    content:
+      "Exploring the great outdoors can be a life-changing experience. Whether you're hiking through forests, climbing mountains, or camping by the lakeside, nature has much to offer.",
+    title: "Adventures in Nature",
+    author: "OutdoorExplorer",
+  },
+  {
+    id: 4,
+    content:
+      "Culinary delights from around the world bring people together. From spicy curries to delicate pastries, the diversity of flavors reflects the richness of cultures globally.",
+    title: "Global Gastronomy",
+    author: "FoodieJourney",
+  },
+  {
+    id: 5,
+    content:
+      "Books have the power to transport us to different worlds, evoke emotions, and expand our horizons. Let's discuss the latest page-turners and timeless classics.",
+    title: "Journey Through Books",
+    author: "BookWorm",
+  },
+  {
+    id: 6,
+    content:
+      "Fitness is not just about physical health; it's a holistic approach to well-being. Join me on a fitness journey that encompasses body, mind, and soul.",
+    title: "Mindful Fitness",
+    author: "WellnessWarrior",
+  },
+  {
+    id: 7,
+    content:
+      "Artistic expression knows no bounds. From vivid paintings to soul-stirring music, let's explore the world of creativity and its impact on our lives.",
+    title: "Unleashing Creativity",
+    author: "ArtisticSoul",
+  },
+  {
+    id: 8,
+    content:
+      "Traveling opens doors to new cultures, languages, and perspectives. Join me as I recount my journeys to far-off lands and the lessons learned along the way.",
+    title: "Wanderlust Chronicles",
+    author: "TravelerTales",
+  },
+  {
+    id: 9,
+    content:
+      "In a rapidly changing world, staying updated with technology is essential. Let's dive into the latest trends, gadgets, and their impact on society.",
+    title: "Tech Trends and Beyond",
+    author: "TechInsider",
+  },
+  {
+    id: 10,
+    content:
+      "From the runway to street fashion, clothing is more than just fabric; it's a form of self-expression. Join me in exploring style, trends, and fashion tips.",
+    title: "Chic and Trendy",
+    author: "Fashionista",
+  },
+  {
+    id: 11,
+    content:
+      "Learning a new language opens doors to communication and cultural understanding. Join me in discussing language learning tips, challenges, and success stories.",
+    title: "Polyglot's Corner",
+    author: "LanguageLover",
+  },
+  {
+    id: 12,
+    content:
+      "The world of finance can be both complex and fascinating. Let's unravel investment strategies, economic trends, and personal finance management.",
+    title: "Mastering Finance",
+    author: "FinancePro",
+  },
+  {
+    id: 13,
+    content:
+      "Photography captures fleeting moments and turns them into lasting memories. Join me in discussing photography techniques, gear, and sharing breathtaking shots.",
+    title: "Through the Lens",
+    author: "Shutterbug",
+  },
+  {
+    id: 14,
+    content:
+      "The realm of science is full of wonders and breakthroughs. Let's delve into the mysteries of the universe, from cutting-edge research to mind-bending theories.",
+    title: "Quest for Knowledge",
+    author: "ScienceEnthusiast",
+  },
+  {
+    id: 15,
+    content:
+      "The world of gaming is an escape into virtual realms filled with adventure and challenges. Join me as we explore the latest games, gaming news, and strategies.",
+    title: "Gamer's Odyssey",
+    author: "GameMaster",
+  },
+  {
+    id: 16,
+    content:
+      "Mindfulness and meditation are keys to inner peace and self-discovery. Let's embark on a journey to calmness, explore meditation techniques, and share insights.",
+    title: "Path to Serenity",
+    author: "MindfulSoul",
+  },
+  {
+    id: 17,
+    content:
+      "The automotive world is a blend of innovation and engineering marvels. Join me in discussing the latest car models, futuristic concepts, and motor enthusiasts' community.",
+    title: "Driven by Wheels",
+    author: "AutoAficionado",
+  },
+  {
+    id: 18,
+    content:
+      "Pets bring joy, companionship, and unconditional love into our lives. Let's share heartwarming pet stories, care tips, and insights into the animal kingdom.",
+    title: "Paws and Whiskers",
+    author: "PetLover",
+  },
+  {
+    id: 19,
+    content:
+      "Education is a lifelong journey that shapes our minds and perspectives. Join me as we explore innovative learning methods, academic insights, and intellectual growth.",
+    title: "Learning Unlimited",
+    author: "EduExplorer",
+  },
+  {
+    id: 20,
+    content:
+      "Nature's beauty is all around us, from breathtaking landscapes to the tiniest creatures. Let's celebrate the wonders of the natural world and discuss conservation efforts.",
+    title: "Nature's Tapestry",
+    author: "NatureEnthusiast",
+  },
+];
+
+module.exports = postsData;
