5b99807d1542fcfeca82cdc3afe99ce1251e50db
maciek367 <maciek2baba2@gmail.com>
2023-08-24 22:16:56 +0200

    typo

--- controllers.test.js
+++ controllers.test.js
@@ -2,27 +2,27 @@ const { getAllPosts, getPostById, createPost, updatePost, deletePost } = require
 
 describe('controllers', () => {
   describe('getAllPosts', () => {
-    it('should be an function', () => {
+    it('should be a function', () => {
       expect(typeof getAllPosts).toBe('function');
     });
   });
   describe('getPostById', () => {
-    it('should be an function', () => {
+    it('should be a function', () => {
       expect(typeof getPostById).toBe('function');
     });
   });
   describe('createPost', () => {
-    it('should be an function', () => {
+    it('should be a function', () => {
       expect(typeof createPost).toBe('function');
     });
   });
   describe('updatePost', () => {
-    it('should be an function', () => {
+    it('should be a function', () => {
       expect(typeof updatePost).toBe('function');
     });
   });
   describe('deletePost', () => {
-    it('should be an function', () => {
+    it('should be a function', () => {
       expect(typeof deletePost).toBe('function');
     });
   });
--- server.test.js
+++ server.test.js
@@ -1,2 +1,8 @@
 const { server } = require("./server");
-const supertest = require("supertest");
+const { request } = require("supertest");
+
+describe("server", () => {
+  it("should be an object", () => {
+    expect(typeof server).toBe("object");
+  });
+});
