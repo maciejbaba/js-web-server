d666f44a8114a58e88cc7e455ef8d33e1ed6f31e
maciek367 <maciek2baba2@gmail.com>
2023-08-24 11:04:30 +0200

    change from db to postsData, change structure

--- Posts.js
+++ Posts.js
@@ -1,5 +1,5 @@
 const Post = require("./Post");
-const postsData = require("./db.js");
+const postsData = require("./postsData.json");
 
 const Posts = postsData.map((post) => {
   return new Post(post.id, post.content, post.title, post.author);
